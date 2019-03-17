b4de068c7bacb0fd40691c5f4fc9bcada6ce57f5
Ángel Pérez Llorente <36485821+agepere@users.noreply.github.com>
2019-03-18 00:47:50 +0100

    First commit

---
+++ servidor/src/mainServer.java
@@ -0,0 +1,9 @@
+public class mainServer {
+    
+    public static void main(String args[]) throws Exception {
+        IntegrityVerifierServer server = new IntegrityVerifierServer();
+        server.runServer();
+    }
+
+}
+
